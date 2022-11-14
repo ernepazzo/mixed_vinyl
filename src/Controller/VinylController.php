5d06c80fc4834e4822dd953a3fde4a7766854c60
Ernesto Mejias Alvarez <ernepazzo1212@gmail.com>
2022-11-13 23:01:16 -0500

    Rutas inteligentes -- Comodines!!!

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -11,7 +11,7 @@ use function Symfony\Component\String\u;
 
 class VinylController extends AbstractController
 {
-    #[Route('/')]
+    #[Route('/', name: 'app_homepage')]
     public function homepage(): Response
     {
         $tracks = [
@@ -30,17 +30,13 @@ class VinylController extends AbstractController
         ]);
     }
 
-    #[Route('/browse/{slug}')]
+    #[Route('/browse/{slug}', name: 'app_browse')]
     public function browse(string $slug = null): Response {
-        if ($slug) {
-            $title = 'Genre: ' . u(str_replace('-', ' ', $slug))->title(true);
-        } else {
-            $title = 'All Genres';
-        }
+        $genre = $slug ? u(str_replace('-', ' ', $slug))->title(true) : null;
 
-        return new Response($title);
-
-//        return new Response('Breakup vinyl? Angsty 90s rock? Browse the collection!');
+        return $this->render('vinyl/browse.html.twig', [
+            'genre' => $genre
+        ]);
     }
 
 }
